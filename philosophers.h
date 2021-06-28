0a94b85d8e6ca5c48aeb31889c1ce701f2573f4b
Nasium Hill <zainullinaaliya45@gmail.com>
2021-06-28 19:05:28 +0300

    mkf

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 20:05:22 by nhill             #+#    #+#             */
-/*   Updated: 2021/06/16 20:15:03 by nhill            ###   ########.fr       */
+/*   Updated: 2021/06/28 19:02:09 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+typedef struct		s_mutex
+{
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	written;
+	//pthread_mutex_t	*forks;
+}					t_mutex;
+
+
 
 #endif
