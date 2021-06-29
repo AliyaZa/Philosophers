6a68a090c67cf49fa512ab36300fc3f50f37ee50
Nasium Hill <zainullinaaliya45@gmail.com>
2021-06-29 19:31:16 +0300

    philo

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: nhill <nhill@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 20:05:22 by nhill             #+#    #+#             */
-/*   Updated: 2021/06/28 19:05:38 by nhill            ###   ########.fr       */
+/*   Updated: 2021/06/29 19:30:57 by nhill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,29 @@ typedef struct		s_mutex
 	//pthread_mutex_t	*forks;
 }					t_mutex;
 
-
-typedef struct	s_arg
+typedef struct		s_arg
 {
 	long	number_of_philosophers;
 	long	time_to_die;
 	long	time_to_eat;
 	long	time_to_sleep;
-}				t_arg;
+	long	number_of_times_each_philosopher_must_eat;
+	int		died;
+}					t_arg;
+
+typedef struct		s_philosophers
+{
+	int				id;
+	long			start_time;
+	long			last_time;
+	t_mutex			*mutex;
+	pthread_mutex_t	left_fork;
+	pthread_mutex_t	right_fork;
+	pthread_t		philosopher;
+}					t_philosophers;
+
+
+
 
 
 #endif
